f89f57cac13c350cb9735c19f01e010b00f8c055
Dreadrith <valorinferno@gmail.com>
2022-11-11 10:29:16 +0300

    root fixes

--- Editor/AutoBounds.cs
+++ Editor/AutoBounds.cs
@@ -11,7 +11,7 @@ namespace DreadScripts.AutoBounds
 {
     public class AutoBounds
     {
-        private const float boundsPercentIncrease = 15;
+        private const float boundsPercentIncrease = 25;
 
         //Sets bounds to auto-calculated dimensions starting from the target
         [MenuItem("DreadTools/Utility/AutoBounds/Auto")]
@@ -20,11 +20,16 @@ namespace DreadScripts.AutoBounds
             //Get Selection
             GameObject go = Selection.activeGameObject;
             if (!go) return;
-            Transform rootTransform = go.transform;
+
+            //Check for animator and to use hips instead of target as root
+            //Always using target as root may make the bounds follow improperly
+            Animator ani = go.GetComponent<Animator>();
+            bool isHuman = ani && ani.avatar && ani.isHuman;
+
+            Transform rootBone = isHuman ? ani.GetBoneTransform(HumanBodyBones.Hips) ?? go.transform : go.transform;
 
             //Get child renderers including disabled
             var renderers = go.GetComponentsInChildren<SkinnedMeshRenderer>(true);
-
 
             //Get Max Extent by getting the biggest dimension
             //Avatars can rotate their armature around meaning this dimension can go in any direction, so reuse it for every dimension
@@ -33,19 +38,21 @@ namespace DreadScripts.AutoBounds
             foreach (var r in renderers)
             {
                 if (!r.sharedMesh) continue;
-                var currentExtent = GetMaxAxis(rootTransform.InverseTransformPoint(r.rootBone.position)) + GetMaxAxis(r.sharedMesh.bounds.size);
+                Transform currentRootBone = r.rootBone ?? r.transform;
+                var currentExtent = GetMaxAxis(rootBone.InverseTransformPoint(currentRootBone.position)) + GetMaxAxis(r.sharedMesh.bounds.size);
                 if (maxExtent < currentExtent) maxExtent = currentExtent;
             }
 
-            //Center out the bounds based on current dimensions
-            Vector3 center = new Vector3(0, maxExtent / 2, 0);
+            //If human, hips should stay the center
+            //Otherwise, center the bounds vertically based on current dimensions
+            Vector3 center = new Vector3(0, isHuman ? 0 : maxExtent / 2, 0);
 
             //Increase area by percentage for safe measure
             maxExtent *= 1 + boundsPercentIncrease / 100;
             Vector3 extents = new Vector3(maxExtent, maxExtent, maxExtent);
 
             //Set auto calculated bounds starting from target as root
-            SetBounds(rootTransform, new Bounds(center, extents));
+            SetBounds(go.transform, rootBone, new Bounds(center, extents));
         }
 
 
@@ -66,15 +73,15 @@ namespace DreadScripts.AutoBounds
             }
 
             //Set the samples bounds start from the top most parent
-            SetBounds(go.transform.root, sampleRenderer.bounds);
+            SetBounds(go.transform.root, sampleRenderer.rootBone, sampleRenderer.bounds);
         }
 
         //Sets all children skinned mesh renderers of the root to the given bounds
-        private static void SetBounds(Transform root, Bounds myBounds)
+        private static void SetBounds(Transform root, Transform rootbone, Bounds myBounds)
         {
             foreach (var r in root.GetComponentsInChildren<SkinnedMeshRenderer>(true))
             {
-                r.rootBone = root;
+                r.rootBone = rootbone;
                 r.localBounds = myBounds;
                 EditorUtility.SetDirty(r);
             }
